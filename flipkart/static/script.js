8ab5fac96044148ad90ca1d0ed78ad8c7b353a95
hemasurya106 <157039161+hemasurya106@users.noreply.github.com>
2024-12-23 13:34:59 +0530

    Delete flipkart directory

--- flipkart/static/script.js
+++
@@ -1,7 +0,0 @@
-function accepted(){
-    
-    let obj = document.querySelector(".Success").style="display:block";
-    
-setTimeout(()=>{obj = document.querySelector(".Success").style="display:none"},2000);
-
-}
